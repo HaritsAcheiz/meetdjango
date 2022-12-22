f3c12740a530607f88525a6e61d51b269a7932ed
HARITS <harits.muhammad.only@gmail.com>
2022-12-23 04:22:19 +0700

    play with models operations and make html document of hompage view success

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from boards.models import Board
+
+
 # Create your views here.
+# def home(request):
+
+    # return HttpResponse('Hello World')
+
+    # boards = Board.objects.all()
+    # boards_name = list()
+    #
+    # for i in boards:
+    #     boards_name.append(i.name)
+    #
+    # response_html = '<br>'.join(boards_name)
+    #
+    # return HttpResponse(response_html)
 def home(request):
-    return HttpResponse('Hello World')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
