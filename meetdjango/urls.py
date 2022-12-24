a78a54abe41e7cb5a3853ba9282aeca951d9b58c
HARITS <harits.muhammad.only@gmail.com>
2022-12-25 04:26:30 +0700

    create topics dynamic url

--- meetdjango/urls.py
+++ meetdjango/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from boards import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('boards/<int:pk>/', views.board_topics),
+    path('admin/', admin.site.urls),
 ]
