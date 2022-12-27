9e72ecfdf933f8f3e56148de5f3cea8f8341fa8a
HARITS <harits.muhammad.only@gmail.com>
2022-12-28 05:47:11 +0700

    new topic test completed

--- boards/tests.py
+++ boards/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from boards.models import Board
+from boards.models import Board, Topic, Post
 from boards.views import home, board_topics, new_topic
 
 
@@ -20,8 +20,6 @@ class HomeTests(TestCase):
 class BoardTopicsTests(TestCase):
     def setUp(self):
         self.board = Board.objects.create(name='Django', description='Django board')
-        url = reverse('home')
-        self.response = self.client.get(url)
 
     def test_board_topics_view_success_status_code(self):
         url = reverse('board_topics', kwargs={'pk': 1})
@@ -38,8 +36,10 @@ class BoardTopicsTests(TestCase):
         self.assertEquals(view.func, board_topics)
 
     def test_home_view_contains_link_to_topic_page(self):
+        homepage_url = reverse('home')
         board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
-        self.assertContains(self.response, f'href="{board_topics_url}"')
+        response = self.client.get(homepage_url)
+        self.assertContains(response, f'href="{board_topics_url}"')
 
     def test_board_topics_view_contains_required_navigation_link(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
@@ -52,9 +52,40 @@ class BoardTopicsTests(TestCase):
 class NewTopicTests(TestCase):
     def setUp(self):
         self.board = Board.objects.create(name='Django', description='Django Board')
-        url = reverse('home')
         self.user = User.objects.create_user(username='john', email='john@doe.com', password='123')
-        self.home_response = self.client.get(url)
+
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk':1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Test message'
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 302)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invalid_post_empty_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.get(url)
