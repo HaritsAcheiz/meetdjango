82b45df6ccff90a46e2da9e255df7e304c5be200
HARITS <harits.muhammad.only@gmail.com>
2022-12-29 03:55:01 +0700

    create form new topic with Django Forms API, test still not passed

--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
+from boards.forms import NewTopicForm
 from boards.models import Board, Topic, Post
 
 
@@ -38,24 +39,42 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
 
-        user = User.objects.first()
+        # before using Django Forms API
+        # subject = request.POST['subject']
+        # message = request.POST['message']
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
+        # user = User.objects.first()
 
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
 
-        return redirect('board_topics', pk=board.pk)
+        # before using Django Forms API
+        # topic = Topic.objects.create(
+        #     subject=subject,
+        #     board=board,
+        #     starter=user
+        # )
+        #
+        # post = Post.objects.create(
+        #     message=message,
+        #     topic=topic,
+        #     created_by=user
+        # )
 
-    return render(request, 'new_topic.html', {'board': board})
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form':form})
